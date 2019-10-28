1f5be1b998392316f00fd952b7121ee5b99c722a
joedegere <degerej@hartwick.edu>
2019-10-28 09:09:13 -0400

    File Updated!

--- Craps.py
+++ Craps.py
@@ -11,27 +11,27 @@ def balance():
     return int(input("> "))
 
 
-bank = balance()
+bigbank = balance()
 
 
-print(f"Enter the amount you want to bet from your bank {bank}")
+print(f"Enter the amount you want to bet from your bank {bigbank}")
 bet = int(input("> "))
 
 
-while bet <= bank:
+while bet <= bigbank:
     roll = random.randint(2,12)
 
     print(f"You rolled a {roll}")
 
     if roll == 7 or roll == 11:
         print("You win!")
-        bank = bank + bet
+        bank = bigbank + bet
         print(f"You now have {bank} in your bank.")
 
     elif roll == 2 or roll == 3 or roll == 12:
         print("You lose!")
-        bank = bank - bet
-        print(f"Now you have {bank} in your bank.")
+        bank = bigbank - bet
+        print(f"Now you have {bigbank} in your bank.")
     else:
         print("This value becomes your point, you must roll this value again to win, or roll a 7 and lose!")
         point = roll
@@ -43,22 +43,22 @@ while bet <= bank:
             roll = random.randint(2,12)
 
         if roll == point:
-            bank = bank + bet
+            bank = bigbank + bet
             print("You win! You rolled point!")
             print(f"You now have {bank} in your bank!")
         else:
-            bank = bank - bet
+            bank = bigbank - bet
             print("You rolled a 7, You Lose!")
     print("If you would like to play again? yes or no")
     choice = input()
     if choice == "yes":
-        print(f"You have {bank} in your bank. How much would you like to bet?")
+        print(f"You have {bigbank} in your bank. How much would you like to bet?")
         bet = int(input("> "))
-        if bet > bank:
+        if bet > bigbank:
             print(f"You can't bet more than what you have in your bank. The game will now end.")
-            bank = 0
+            bigbank = 0
     else:
-        bank = 0
+        bigbank = 0
 
 while bet <= 0:
     print("Invalid input, try again")
